e42fda25414663a30b22f9d528302d84cf13a6e6
meenapv <meenu094@gmail.com>
2017-04-13 10:09:24 -0500

    new commit

---
+++ src/test/java/com/themesoft/tests/DocumentTypePayStub_Paystub1_Test.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.themesoft.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.json.JSONObject;
+import org.junit.Test;
+
+import com.themesoft.service.FileService;
+public class DocumentTypePayStub_Paystub1_Test {
+    
+    @Test
+    public void testPayStub_1(){
+    
+        try{
+           JSONObject response=FileService.submitFile("paystub-1.pdf");
+           System.out.println(response.toString());
+           
+           assertEquals("Pay Stub Earning Periodic Unit Count","",response.get("1720"));
+           assertEquals("Pay Stub Earning Rate Amount","",response.get("1719"));
+           assertEquals("Pay Stub Periodic Income Amount","1758.33",response.get("1812"));
+           assertEquals("Pay Stub Year to Date Income Amount","3517.62",response.get("1813"));
+           assertEquals("Income Source End Date","2/01/16",response.get("1687"));
+           assertEquals("Pay Stub Periodic Earning Amount","1267.18",response.get("1733"));
+           assertEquals("Pay Stub Earning Type","REG CASHOP",response.get("1718"));
+           
+        }
+        catch(Exception e){
+        e.printStackTrace();
+        }
+    }
+    
+}
